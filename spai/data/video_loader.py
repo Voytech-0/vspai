df43fb6848c07343775918e3ded6829fdba89438
Wojciech Trejter <47249798+Voytech-0@users.noreply.github.com>
2025-04-21 11:13:26 +0200

    If file not readable, return empty image

--- spai/data/video_loader.py
+++ spai/data/video_loader.py
@@ -5,10 +5,16 @@ from PIL import Image
 
 
 def get_frame(filepath: str, index: int):
-    vr = decord.VideoReader(filepath, ctx=decord.cpu(0))
+    # try catch
+    try:
+        vr = decord.VideoReader(filepath, ctx=decord.cpu(0))
+        image = vr[index].asnumpy()
+        image = Image.fromarray(image)
+    except Exception as e:
+        print("Image not read properly: ", filepath)
+        image = Image.new("RGB", (128, 128), (0, 0, 0))
+
     # tensor = torch.as_tensor(vr[0].asnumpy())
-    image = vr[index].asnumpy()
-    image = Image.fromarray(image)
     return image
 
 def get_num_frames(filepath: str):
