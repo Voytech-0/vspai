f92e019d1317e9408d7d652669541967231d3876
Wojciech Trejter <47249798+Voytech-0@users.noreply.github.com>
2025-04-27 10:08:30 +0200

    Memory-managed video_loader.py

--- spai/data/video_loader.py
+++ spai/data/video_loader.py
@@ -3,22 +3,20 @@ import torch
 from PIL import Image
 
 
-
 def get_frame(filepath: str, index: int):
-    # try catch
     try:
-        vr = decord.VideoReader(filepath, ctx=decord.cpu(0))
-        image = vr[index].asnumpy()
-        image = Image.fromarray(image)
+        with decord.VideoReader(filepath, ctx=decord.cpu(0)) as vr:
+            image = vr[index].asnumpy()
+            image = Image.fromarray(image)
     except Exception as e:
         print("Image not read properly: ", filepath)
         image = Image.new("RGB", (128, 128), (0, 0, 0))
 
-    # tensor = torch.as_tensor(vr[0].asnumpy())
     return image
 
-def get_num_frames(filepath: str):
-    vr = decord.VideoReader(filepath, ctx=decord.cpu(0))
 
-    return len(vr)
-    
+def get_num_frames(filepath: str):
+    with decord.VideoReader(filepath, ctx=decord.cpu(0)) as vr:
+        len_vr = len(vr)
+
+    return len_vr
