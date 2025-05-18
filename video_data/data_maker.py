30e7c54180f5d1bc4008082a78dcef40e8c3be27
Wojciech Trejter <47249798+Voytech-0@users.noreply.github.com>
2025-05-18 09:07:05 +0100

    data_maker checks for corrupted files

--- video_data/data_maker.py
+++ video_data/data_maker.py
@@ -1,6 +1,44 @@
 import click
 import pandas as pd
+import numpy as np
 import os
+import decord
+import multiprocessing
+
+
+def one_core_remove_corrupted(split):
+    split = list(split)
+    for filename in split[:]:
+        try:
+            decord.VideoReader(filename, ctx=decord.cpu(0))
+        except Exception as e:
+            print(f"Corrupted file: {filename}, Error: {e}")
+            split.remove(filename)
+    return np.asarray(split)
+
+
+def remove_corrupted(df: pd.DataFrame):
+    n_cores = os.cpu_count()
+    df_np = df['image'].to_numpy()
+    df_split = np.array_split(df_np, n_cores)
+
+    # Create a pool of workers
+    pool = multiprocessing.Pool(n_cores)
+
+    # Map the function to the data chunks
+    results = pool.map(one_core_remove_corrupted, df_split)
+
+    pool.close()
+    pool.join()
+
+
+    # Combine the results
+    df_np = np.concatenate(results)
+
+    # Update the DataFrame
+    df = df[df['image'].isin(df_np)]
+
+    return df
 
 
 @click.group()
@@ -12,10 +50,15 @@ def cli() -> None:
 @click.option('--data-folder', type=click.Path(exists=True, file_okay=False), required=True)
 @click.option('--output-folder', type=click.Path(file_okay=False), required=False, default='.')
 @click.option('--fake', is_flag=True, default=False)
-@click.option('--split', type=click.Choice(['train', 'val', 'test']), default='test')
-def make_csv(data_folder, output_folder, fake, split):
+@click.option('--split', type=click.Choice(['train', 'val', 'test']), default='train')
+@click.option("--rm-corrupted", is_flag=True, default=False)
+@click.option("--file-number-limit", type=int, default=10100)
+def make_csv(data_folder, output_folder, fake, split, rm_corrupted, file_number_limit):
     output_name = 'fake_' if fake else 'real_'
-    output_name += data_folder.replace('/', '_') + '.csv' + '.csv'
+    if '/' not in data_folder:
+        output_name += data_folder + '.csv'
+    else:
+        output_name += data_folder.split('/')[-1] + '.csv'
     # create df with  image,class,split headers
     df = pd.DataFrame(columns=['image', 'class', 'split'])
     class_value = 1 if fake else 0
@@ -23,7 +66,17 @@ def make_csv(data_folder, output_folder, fake, split):
         for file in files:
             filename = (os.path.join(root, file))
             # add row to df
-            df = pd.concat([df, pd.DataFrame({'image': [filename], 'class': [class_value], 'split': [split]})])
+            if filename.endswith('.mp4'):
+                df = pd.concat([df, pd.DataFrame({'image': [filename], 'class': [class_value], 'split': [split]})])
+
+    # shuffle
+    df = df.sample(frac=1).reset_index(drop=True)
+    if file_number_limit is not None:
+        df = df[:file_number_limit]
+
+    if rm_corrupted:
+        df = remove_corrupted(df)
+
     df.to_csv(os.path.join(output_folder, output_name), index=False)
 
 
